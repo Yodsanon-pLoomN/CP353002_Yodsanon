4aabe69fdc974b628902a20551e47bcf7d715094
Yodsanon-pLoomN <pLoomN321654987@gmail.com>
2025-06-25 16:08:08 +0700

    add getter and setter

--- Student.java
+++ Student.java
@@ -1,9 +1,16 @@
 class Student{
     private String  name;
     public Student(){
-
     }
-    
+    public Student(String name){
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
 
 
 
